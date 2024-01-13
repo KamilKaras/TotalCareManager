1cdb3323030aa4876abf2295f7e96d4060776146
Kamil Karasiewicz <kkarasiewicz@vanto.pl>
2024-01-13 10:49:45 +0100

    Code refactor

--- backend/src/Modules/UserAccess/Domain/Clubs/Enumerations/ClubType.cs
+++ backend/src/Modules/UserAccess/Domain/Clubs/Enumerations/ClubType.cs
@@ -5,7 +5,7 @@ namespace UserAccess.Domain.Clubs.Enumerations
     public sealed class ClubType : Enumeration<ClubType>
     {
         public static readonly ClubType FightClub = new(1, "Klub sportów walki");
-        public static readonly ClubType SportClub = new(2, "Klub sportowe");
+        public static readonly ClubType SportClub = new(2, "Klub sportowy");
         public static readonly ClubType Gym = new(3, "Siłownia");
         public static readonly ClubType FitnessClub = new(3, "Klub fitnes");
         public static readonly ClubType Kindergarden = new(4, "Przedszkole");
